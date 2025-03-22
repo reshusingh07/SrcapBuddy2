2922f9f5f7d6ad98b366b002d860c5fec9154498
Reshu Singh <reshu70173@gmail.com>
2025-03-22 12:05:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'scrap-blue': '#1e3a8a',
+        'scrap-orange': '#ea580c',
+      },
+    },
+  },
+  plugins: [],
+}
